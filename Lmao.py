f1c4eccf93834ae68b9a81169a5b375b009647cc
Kuchizu <70284260+Kuchizu@users.noreply.github.com>
2020-09-30 14:57:04 +0500

    Update Lmao.py

--- Lmao.py
+++ Lmao.py
@@ -42,7 +42,7 @@ class JacquesTwoMod(loader.Module):
 		else:
 			txt = utils.get_args_raw(message)
 		await message.edit("<b>Лолизируем...</b>")
-		pic = requests.get("https://i.ibb.co/0DvPJzB/fuck.png")
+		pic = requests.get("https://i.ibb.co/Z6Lzw1j/shit.png")
 		pic.raw.decode_content = True
 		img = Image.open(io.BytesIO(pic.content)).convert("RGB")
  
